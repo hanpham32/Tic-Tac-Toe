0f6a6b2f1750cddd0af52f5c96e203b99fc2f107
Rosenwaldm <s-rosenwaldm@bsd405.org>
2019-02-06 09:29:00 -0800

    Completed input with tokenizer

--- src/input.java
+++ src/input.java
@@ -1,4 +1,19 @@
-
+import java.util.Scanner;
+import java.util.StringTokenizer; 
 public class input {
+	public static void main(String args[]) { 
+		System.out.println("Enter the position: "); 
+		Scanner input = new Scanner(System.in);
+		String positions = input.nextLine(); 
 
+		StringTokenizer st = new StringTokenizer(positions, ",");
+		
+		String first = st.nextToken(); 
+		String second = st.nextToken(); 
+		
+		int row = Integer.parseInt(first); 
+		int column = Integer.parseInt(second);
+		
+		
+	}
 }
