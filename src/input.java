04447bd1b6232f15de9c95ff2adee0cabe88b18b
Rosenwaldm <s-rosenwaldm@bsd405.org>
2019-02-14 09:45:34 -0800

    checking fixes and comments

--- src/input.java
+++ src/input.java
@@ -2,36 +2,35 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class input {
-	private int row;
-	private int column;
-
+	private int row; // set variable row
+	
+	private int column; // set variable column 
+	
+	// creating point method 
 	public void setPoint() {
+		
+		// getting user input for point 	
 		System.out.println("Enter the position: ");
 		Scanner input = new Scanner(System.in);
 		String positions = input.nextLine();
 
+		// tokenize input -- split up row and column 
 		StringTokenizer st = new StringTokenizer(positions, ",");
 
+		// assigning tokens to variables 
 		String first = st.nextToken();
 		String second = st.nextToken();
 
+		// parsing variables to row and column and making them into numbers arrays can use 
 		row = Integer.parseInt(first) - 1;
 		column = Integer.parseInt(second) - 1;
-
+		checkPoint(); 
+	}
+	
+	public void checkPoint() {
 		if (row > 2 || column > 2) {
 			System.out.println("Invalid input please re-enter a point between 1 and 3");
-
-			System.out.println("Enter the position: ");
-			Scanner input2 = new Scanner(System.in);
-			String positions2 = input.nextLine();
-
-			StringTokenizer st2 = new StringTokenizer(positions2, ",");
-
-			String first2 = st2.nextToken();
-			String second2 = st2.nextToken();
-
-			row = Integer.parseInt(first2) - 1;
-			column = Integer.parseInt(second2) - 1;
+			setPoint(); 
 		}
 	}
 
